9b2a5bdec0224ed1ec169e529b8104f65b42d112
Geoff <picardrulez@gmail.com>
2017-07-19 10:49:05 -0400

    initial commit

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type readStruct struct {
+	Env     string `json:"env"`
+	Version string `json:"version"`
+}
+
+func main() {
+	res, err := http.Get("http://localhost:8000")
+	if err != nil {
+		panic(err.Error())
+	}
+	//body, err := ioutil.ReadAll(res.Body)
+	//if err != nil {
+	//		panic(err.Error())
+	//	}
+	decoder := json.NewDecoder(res.Body)
+	var jsonResponse readStruct
+	err = decoder.Decode(&jsonResponse)
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Println(jsonResponse.Version)
+}
